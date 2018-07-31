974cf9370e6830085a6fa347b68f84bee4e41e9e
gferra02 <gferra02@mail.bbk.ac.uk>
2018-07-31 23:22:00 +0100

    Added rhyme

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,105 +2,14 @@
 console.log('hello outside document ready');
 
 $(document).ready(function() {
-
-    // d3.json('/assets/json/json_sample.json').then(function(d) {
-  //     var   char_lines = [],
-  //           height = 400,
-  //           width = 1080;
-
-  //     var   tempColor,
-  //           yScale,
-  //           xScale,
-  //           colors,
-  //           tooltip,
-  //           myChart;
-
-
-  //     for (var i = 0; i < d.cantica[0].canto[0].tercet[0].lines.length; i++) {
-  //       char_lines.push(d.cantica[0].canto[0].tercet[0].lines[i].chars);
-  //     }
-
-  //     yScale = d3.scaleLinear()
-  //       .domain([0, d3.max(char_lines)])
-  //       .range([0,height]);
-
-  //     xScale = d3.scaleBand()
-  //       .domain(char_lines)
-  //       .paddingInner(.3)
-  //       .paddingOuter(.1)
-  //       .range([0, width])
-
-  //     colors = d3.scaleLinear()
-  //       .domain([0, char_lines.length *.33,
-  //                   char_lines.length *.66,
-  //                   char_lines.length
-  //                   ])
-  //       .range(['#B58929', '#C61C6F',
-  //               '#268BD2', '#85992C'])
-
-  //     tooltip = d3.select('body')
-  //       .append('div')
-  //       .style('position', 'absolute')
-  //       .style('padding', '0 5px')
-  //       .style('background', 'white')
-  //       .style('opacity', 0);
-
-  //     myChart = d3.select('#viz').append('svg')
-  //       .attr('width', width)
-  //       .attr('height', height)
-  //       .selectAll('rect').data(char_lines)
-  //       .enter().append('rect')
-  //         .attr('fill', function(d, i) {
-  //           return colors(i)
-  //         })
-  //         .attr('width', function(d) {
-  //           return xScale.bandwidth();
-  //         })
-  //         .attr('height', 0)
-  //         .attr('x', function(d) {
-  //           return xScale(d);
-  //         })
-  //         .attr('y', height)
-          
-  //         .on('mouseover', function(d) {
-  //           tooltip.transition().duration(200)
-  //             .style('opacity', .9)
-  //           tooltip.html(d)
-  //             .style('left', (d3.event.pageX -35) + 'px')
-  //             .style('top', (d3.event.pageY -30) + 'px')
-  //           tempColor = this.style.fill;
-  //           d3.select(this)
-  //             .style('fill', 'yellow')
-  //         })
-
-  //         .on('mouseout', function(d) {
-  //           d3.select(this)
-  //             .style('fill', tempColor)
-  //         });
-
-  //     myChart.transition()
-  //       .attr('height', function(d) {
-  //         return yScale(d);
-  //       })
-  //       .attr('y', function(d) {
-  //         return height - yScale(d);
-  //       })
-  //       .delay(function(d, i) {
-  //         return i * 20;
-  //       })
-  //       .duration(1000)
-  //       .ease(d3.easeBounceOut)
-  // })
-
-    // console.log('ebfore d3');
-
-    d3.json('/assets/json/json_sample.json').then(function(d) {
+        d3.json('/assets/json/json_sample.json').then(function(d) {
 
         console.log(d.cantica[0].canto[0].tercet[0].lines[0].text);
 
         var char_lines = [],
             text_lines = [],
             line_numbers = [],
+            rhymes = [],
             margin = { top: 0, right: 0, bottom: 30, left: 20 }
             height = 400 - margin.top - margin.bottom,
             width = 1080 - margin.left - margin.right;
@@ -122,6 +31,7 @@ $(document).ready(function() {
             char_lines.push(d.cantica[0].canto[0].tercet[0].lines[i].chars - 1); // -1 to count for the new line char at the end of every line
             text_lines.push(d.cantica[0].canto[0].tercet[0].lines[i].text);
             line_numbers.push(d.cantica[0].canto[0].tercet[0].lines[i].line_number);
+            rhymes.push(d.cantica[0].canto[0].tercet[0].lines[i].rhyme)
         }
 
         yScale = d3.scaleLinear()
@@ -171,7 +81,7 @@ $(document).ready(function() {
               // .attr('width', function(d, i) {
               //   return xScale(i);
               // })
-              .attr('width','4px')
+              .attr('width','5px')
               .attr('height', 0)
               .attr('x', function(d, i) {
                 return xScale(i);
@@ -181,7 +91,10 @@ $(document).ready(function() {
                 tooltip.transition().duration(200)
                   .style('opacity', .9)
                 tooltip.html(
-                  '<div>chars: ' + d + '<br>line: ' + line_numbers[i] + '<br>text: ' + text_lines[i] + '</div>'
+                  '<div><strong>chars:</strong> ' + d +
+                  '<br><strong>line:</strong> ' + line_numbers[i] + 
+                  '<br><strong>text:</strong> ' + text_lines[i] + 
+                  '<br><strong>rhyme:</strong> ' + rhymes[i] + '</div>'
                 )
                   .style('left', (d3.event.pageX) + 'px')
                   .style('top', (d3.event.pageY) + 'px')
